951c368f921696b643f6b2c3664d0d63c2e44aad
soh342 <soh342@gatech.edu>
2021-07-12 16:02:09 -0400

    updated to new scHybridNMF formulation, added to readme

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 single-cell hybrid NMF package used to bicluster gene expression and spatial location data of single cells.
 
 Files include:
-- **scHybridNMF**: a combined NMF scheme that takes a pair of 2-by-n location matrix and m-by-n gene expression matrix of single cell data.
-- **vis_clusters**: a script to save side-by-side dot plots of cell locations and t-SNE plots of gene expression data. The dots are color-coded based on cluster membership.
+- **scHybridNMF**: a combined NMF scheme that takes:
+	-- a 2-by-n location matrix, 
+	-- an m-by-n gene expression matrix of single cell data, 
+	-- the number of clusters k,
+	-- (OPTIONAL) a location-gene expression weighting term alpha,
+	-- (OPTIONAL) the tolerance level for the normalized KKT residual stopping criterion,
+	-- (OPTIONAL) a maximum number of iterations.
 - All other files are for running NMF.
--- scHybridNMF.m
+++ scHybridNMF.m
@@ -1,13 +1,18 @@
 % Given A, an m-by-n data matrix, and L, a 2-by-n location matrix,
-% use block coordinate descent to compute
-%
-% min_{W_A,H_A} ||A-W_A*H_A||_F^2 + alpha*||H_A - H_A circ H_L||_F^2 
-% + beta*||W_A||_F^2 + gamma*sum_{i=1}^n ||H_A(:,i)||_F^2
+% use block coordinate descent to compute:
+% min_{W_A,H_A} ||A-W_A*H_A||_F^2 + alpha||H_A - H_A circ H_L||_F^2
 % subject to W_A, H_A>=0
-function [H_A, W_A, nmf_labels, km_labels] = scHybridNMF(A, L, k, H_A, H_L, W_A, W_L, alpha, beta, gamma)
+function [H_A, W_A, nmf_ids, j_ids] = scHybridNMF(A, L, k, alpha, tol, num_iters)
     % Get optional arguments.
-    num_iters = 50;
-    tol = 0.5;
+    if isempty(num_iters)
+        num_iters = 500;
+    end
+    if isempty(alpha)
+        alpha = 0.2;
+    end
+    if isempty(tol)
+        tol = 0.01;
+    end
 
     %initializes norm differences
     proj_norms = zeros(num_iters,1);
@@ -16,60 +21,39 @@ function [H_A, W_A, nmf_labels, km_labels] = scHybridNMF(A, L, k, H_A, H_L, W_A,
     [m,n] = size(A);
 
     %initialize W_A (m-by-k), H_A (k-by-n) by nmf
-    if isempty(W_A) && isempty(H_A)
-        [W_A,H_A] = nmf(A,k,'type','sparse','alpha',beta,'beta',gamma);
-        [~,nmf_labels] = max(H_A);
-        while numel(unique(nmf_labels)) < k
-            [W_A,H_A] = nmf(A,k,'type','sparse','alpha',beta,'beta',gamma);
-            [~,nmf_labels] = max(H_A);
-        end
-    end
-    [~,nmf_labels] = max(H_A);
+    beta = m / norm(A,'fro')^2;
+    gamma = n / norm(A,'fro')^2;
+    [~,H_A] = nmf(A,k,'type','sparse','alpha',beta,'beta',gamma);
+    [~,nmf_ids] = max(H_A);
 
     %initialize W_L (2-by-k), H_L (k-by-n) by kmeans
-    if isempty(H_L)
-        start_centroids = zeros(k,2);
-        for k_i = 1:k
-            k_group = find(nmf_labels == k_i);
-            start_centroids(k_i,:) = mean(L(k_group,:));
-        end
-        [km_labels,W_L] = kmeans(L,k,'Start',start_centroids);
-        H_L = double(bsxfun(@eq, km_labels(:), 1:max(km_labels)));
-        H_L = H_L.';
-        W_L = W_L.';
-    else
-        [~,km_labels] = max(H_L);
-    end
+    [j_ids,W_L] = kmeans(L.',k);
+    H_L = double(bsxfun(@eq, j_ids(:), 1:max(j_ids)));
+    H_L = H_L.';
+    W_L = W_L.';
+    [~,j_ids] = max(H_L);
     H_L = confidence_hl(L.',W_L,H_L);
     C = ones(size(H_L)) - H_L;
-
-    %set parameters
-    if isempty(alpha)
-        alpha = norm(A,'fro')^2 / norm(H_A,'fro')^2;
-    end
-    if isempty(beta)
-        beta = mean(A(:)) * m / norm(A,'fro')^2;
-    end
-    if isempty(gamma)
-        gamma = n / norm(A,'fro')^2;
-    end
-
+    
     grad_H_A = zeros(k,n);
+    W_A = rand(m,k);
     for i = 1 : num_iters
+        %bcd for W_A
+        left_W_A = H_A * H_A.';
+        right_W_A = H_A * A.';
+        W_A = nnlsm_blockpivot(left_W_A, right_W_A, 1, W_A.');
+        W_A = W_A.';
+        
         %bcd for H_A
-        left_H_A = W_A.' * W_A + gamma * ones(k);
+        left_H_A = W_A.' * W_A;
         right_H_A = W_A.' * A;
         for j = 1:n
             left_H_A_j = left_H_A + alpha * diag(C(:,j)).^2;
             [H_A(:,j),grad_H_A(:,j)] = nnlsm_blockpivot(left_H_A_j, right_H_A(:,j), 1, H_A(:,j));
         end
         
-        %bcd for W_A
-        left_W_A = H_A * H_A.' + beta * eye(k);
+        left_W_A = H_A * H_A.';
         right_W_A = H_A * A.';
-        W_A = nnlsm_blockpivot(left_W_A, right_W_A, 1, W_A.');
-        W_A = W_A.';
-        
         grad = [grad_H_A, left_W_A * W_A.' - right_W_A];
         proj_norms(i) = norm(grad(grad<=0|[H_A,W_A.']>0));
         if proj_norms(i) < tol * proj_norms(1)
